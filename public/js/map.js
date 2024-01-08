4c8718c58169a5f2fafb83e33af8366902636407
Hijab <neelumneelum41@gmail.com>
2024-01-08 22:45:10 +0530

    commit changes

--- public/js/map.js
+++ public/js/map.js
@@ -1,13 +1,16 @@
-mapboxgl.accessToken =
-    "pk.eyJ1IjoiZGVsdGEtc3R1ZHVlbnQiLCJhIjoiY2xvMDk0MTVhMTJ3ZDJrcGR5ZDFkaHl4ciJ9.Gj2VU1wvxc7rFVt5E4KLOQ";
+mapboxgl.accessToken = mapToken;
+// mapboxgl.accessToken =
+//     "pk.eyJ1IjoiZGVsdGEtc3R1ZHVlbnQiLCJhIjoiY2xvMDk0MTVhMTJ3ZDJrcGR5ZDFkaHl4ciJ9.Gj2VU1wvxc7rFVt5E4KLOQ";
 const map = new mapboxgl.Map({
     container: "map",
     style: "mapbox://styles/mapbox/streets-v12",
-    center: [...coordinates],
+    center: [77.1025, 28.7041],
     zoom: 8,
 });
 
-const marker = new mapboxgl.Marker({ color: "red" }).setLngLat([...coordinates])
-.setPopup(new mapboxgl.Popup({offset: 25})
-.setHTML("<h1>Welcome to WanderLust!</h1>"))
-.addTo(map);
+// console.log(coordinates);
+
+// const marker = new mapboxgl.Marker().setLngLat(coordinates)
+// .setPopup(new mapboxgl.Popup({offset: 25})
+// .setHTML("<h1>Welcome to WanderLust!</h1>"))
+// .addTo(map);
