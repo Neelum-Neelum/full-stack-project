cae07cf27c17c91b53c0c614164838f78ba12a2e
Hijab <neelumneelum41@gmail.com>
2024-01-17 23:24:11 +0530

    deleted file

--- public/js/map.js
+++ public/js/map.js
@@ -1,16 +1,13 @@
 mapboxgl.accessToken = mapToken;
-// mapboxgl.accessToken =
-//     "pk.eyJ1IjoiZGVsdGEtc3R1ZHVlbnQiLCJhIjoiY2xvMDk0MTVhMTJ3ZDJrcGR5ZDFkaHl4ciJ9.Gj2VU1wvxc7rFVt5E4KLOQ";
 const map = new mapboxgl.Map({
     container: "map",
     style: "mapbox://styles/mapbox/streets-v12",
-    center: [77.1025, 28.7041],
+    center: listing.geometry.coordinates,
     zoom: 8,
 });
 
-// console.log(coordinates);
 
-// const marker = new mapboxgl.Marker().setLngLat(coordinates)
-// .setPopup(new mapboxgl.Popup({offset: 25})
-// .setHTML("<h1>Welcome to WanderLust!</h1>"))
-// .addTo(map);
+const marker = new mapboxgl.Marker({color: "red"}).setLngLat(listing.geometry.coordinates)
+.setPopup(new mapboxgl.Popup({offset: 25})
+.setHTML(`<h1>${listing.location}</h1>`))
+.addTo(map);
